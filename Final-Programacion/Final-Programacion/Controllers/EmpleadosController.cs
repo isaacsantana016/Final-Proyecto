a4f767db3486058700bbb94041bfbfa9a22704e1
Schery05 <yamilet.schery05@gmail.com>
2019-04-09 04:12:32 -0400

    MODULOS DE MANTENIMIENTO
    AQUI AGREGUE TODOS LOS MODULOS DE MANTENIMIENTO

---
+++ Final-Programacion/Final-Programacion/Controllers/EmpleadosController.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Final_Programacion.Models;
+
+namespace Final_Programacion.Controllers
+{
+    public class EmpleadosController : Controller
+    {
+        private Model1Container db = new Model1Container();
+
+        public ActionResult Activos(string searching)
+        {
+            var empleados = db.EmpleadosSet.Include(e => e.Departamentos).Include(e => e.Cargos);
+            return View(empleados.Where(x => x.Nombre.Contains(searching) || searching == null).ToList());
+        }
+
+        public ActionResult Inactivos(string searching)
+        {
+            var empleados = db.EmpleadosSet.Include(e => e.Departamentos).Include(e => e.Cargos);
+            return View(empleados.Where(x => x.Nombre.Contains(searching) || searching == null).ToList());
+        }
+        // GET: Empleados
+        public ActionResult Index()
+        {
+            var empleadosSet = db.EmpleadosSet.Include(e => e.Departamentos).Include(e => e.Cargos);
+            return View(empleadosSet.ToList());
+        }
+        public ActionResult Procesos()
+        {
+            var empleados = db.EmpleadosSet.Include(e => e.Departamento).Include(e => e.Cargo);
+            return View(empleados.ToList());
+        }
+
+
+
+        // GET: Empleados/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Empleados empleados = db.EmpleadosSet.Find(id);
+            if (empleados == null)
+            {
+                return HttpNotFound();
+            }
+            return View(empleados);
+        }
+
+        // GET: Empleados/Create
+        public ActionResult Create()
+        {
+            ViewBag.DepartamentosId = new SelectList(db.Departamentos, "Id", "Nombre");
+            ViewBag.CargosId = new SelectList(db.CargosSet, "Id", "Cargo");
+            return View();
+        }
+
+        // POST: Empleados/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Codigo_Empleado,Nombre,Apellido,Telefono,Fecha_Ingreso,Salario,Estatus,DepartamentosId,CargosId")] Empleados empleados)
+        {
+            if (ModelState.IsValid)
+            {
+                db.EmpleadosSet.Add(empleados);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.DepartamentosId = new SelectList(db.Departamentos, "Id", "Nombre", empleados.DepartamentosId);
+            ViewBag.CargosId = new SelectList(db.CargosSet, "Id", "Cargo", empleados.CargosId);
+            return View(empleados);
+        }
+
+        // GET: Empleados/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Empleados empleados = db.EmpleadosSet.Find(id);
+            if (empleados == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.DepartamentosId = new SelectList(db.Departamentos, "Id", "Nombre", empleados.DepartamentosId);
+            ViewBag.CargosId = new SelectList(db.CargosSet, "Id", "Cargo", empleados.CargosId);
+            return View(empleados);
+        }
+
+        // POST: Empleados/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Codigo_Empleado,Nombre,Apellido,Telefono,Fecha_Ingreso,Salario,Estatus,DepartamentosId,CargosId")] Empleados empleados)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(empleados).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.DepartamentosId = new SelectList(db.Departamentos, "Id", "Nombre", empleados.DepartamentosId);
+            ViewBag.CargosId = new SelectList(db.CargosSet, "Id", "Cargo", empleados.CargosId);
+            return View(empleados);
+        }
+
+        // GET: Empleados/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Empleados empleados = db.EmpleadosSet.Find(id);
+            if (empleados == null)
+            {
+                return HttpNotFound();
+            }
+            return View(empleados);
+        }
+
+        // POST: Empleados/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Empleados empleados = db.EmpleadosSet.Find(id);
+            db.EmpleadosSet.Remove(empleados);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Final-Programacion/Final-Programacion/Models/Model1.edmx.sql
@@ -0,0 +1,275 @@
+
+-- --------------------------------------------------
+-- Entity Designer DDL Script for SQL Server 2005, 2008, 2012 and Azure
+-- --------------------------------------------------
+-- Date Created: 04/09/2019 03:07:06
+-- Generated from EDMX file: C:\Users\yamil\Documents\GitHub\Final-Proyecto\Final-Programacion\Final-Programacion\Models\Model1.edmx
+-- --------------------------------------------------
+
+SET QUOTED_IDENTIFIER OFF;
+GO
+USE [ISAAC-FINAL];
+GO
+IF SCHEMA_ID(N'dbo') IS NULL EXECUTE(N'CREATE SCHEMA [dbo]');
+GO
+
+-- --------------------------------------------------
+-- Dropping existing FOREIGN KEY constraints
+-- --------------------------------------------------
+
+
+-- --------------------------------------------------
+-- Dropping existing tables
+-- --------------------------------------------------
+
+
+-- --------------------------------------------------
+-- Creating all tables
+-- --------------------------------------------------
+
+-- Creating table 'EmpleadosSet'
+CREATE TABLE [dbo].[EmpleadosSet] (
+    [Id] int IDENTITY(1,1) NOT NULL,
+    [Codigo_Empleado] int  NOT NULL,
+    [Nombre] nvarchar(max)  NOT NULL,
+    [Apellido] nvarchar(max)  NOT NULL,
+    [Telefono] nvarchar(max)  NOT NULL,
+    [Fecha_Ingreso] datetime  NOT NULL,
+    [Salario] float  NOT NULL,
+    [Estatus] bit  NOT NULL,
+    [DepartamentosId] int  NOT NULL,
+    [CargosId] int  NOT NULL
+);
+GO
+
+-- Creating table 'Departamentos'
+CREATE TABLE [dbo].[Departamentos] (
+    [Id] int IDENTITY(1,1) NOT NULL,
+    [Codigo_Departamento] int  NOT NULL,
+    [Nombre] nvarchar(max)  NOT NULL
+);
+GO
+
+-- Creating table 'CargosSet'
+CREATE TABLE [dbo].[CargosSet] (
+    [Id] int IDENTITY(1,1) NOT NULL,
+    [Cargo] nvarchar(max)  NOT NULL
+);
+GO
+
+-- Creating table 'Cal_NominaSet'
+CREATE TABLE [dbo].[Cal_NominaSet] (
+    [Id] int IDENTITY(1,1) NOT NULL,
+    [Ano] nvarchar(max)  NOT NULL,
+    [Mes] nvarchar(max)  NOT NULL,
+    [Monto_Total] int  NOT NULL,
+    [EmpleadosId] int  NOT NULL
+);
+GO
+
+-- Creating table 'LicenciasSet'
+CREATE TABLE [dbo].[LicenciasSet] (
+    [Id] int IDENTITY(1,1) NOT NULL,
+    [Desde] datetime  NOT NULL,
+    [Hasta] datetime  NOT NULL,
+    [Motivo] nvarchar(max)  NOT NULL,
+    [Comentario] nvarchar(max)  NOT NULL,
+    [EmpleadosId] int  NOT NULL
+);
+GO
+
+-- Creating table 'PermisosSet'
+CREATE TABLE [dbo].[PermisosSet] (
+    [Id] int IDENTITY(1,1) NOT NULL,
+    [Desde] datetime  NOT NULL,
+    [Hasta] datetime  NOT NULL,
+    [Comentario] nvarchar(max)  NOT NULL,
+    [EmpleadosId] int  NOT NULL
+);
+GO
+
+-- Creating table 'VacacionesSet'
+CREATE TABLE [dbo].[VacacionesSet] (
+    [Id] int IDENTITY(1,1) NOT NULL,
+    [Desde] datetime  NOT NULL,
+    [Hasta] datetime  NOT NULL,
+    [Ano_Corres] nvarchar(max)  NOT NULL,
+    [Comentario] nvarchar(max)  NOT NULL,
+    [EmpleadosId] int  NOT NULL
+);
+GO
+
+-- Creating table 'Salida_EmpleadosSet'
+CREATE TABLE [dbo].[Salida_EmpleadosSet] (
+    [Id] int IDENTITY(1,1) NOT NULL,
+    [Tipo_Salida] nvarchar(max)  NOT NULL,
+    [Motivo] nvarchar(max)  NOT NULL,
+    [Fecha_Salida] datetime  NOT NULL,
+    [EmpleadosId] int  NOT NULL
+);
+GO
+
+-- --------------------------------------------------
+-- Creating all PRIMARY KEY constraints
+-- --------------------------------------------------
+
+-- Creating primary key on [Id] in table 'EmpleadosSet'
+ALTER TABLE [dbo].[EmpleadosSet]
+ADD CONSTRAINT [PK_EmpleadosSet]
+    PRIMARY KEY CLUSTERED ([Id] ASC);
+GO
+
+-- Creating primary key on [Id] in table 'Departamentos'
+ALTER TABLE [dbo].[Departamentos]
+ADD CONSTRAINT [PK_Departamentos]
+    PRIMARY KEY CLUSTERED ([Id] ASC);
+GO
+
+-- Creating primary key on [Id] in table 'CargosSet'
+ALTER TABLE [dbo].[CargosSet]
+ADD CONSTRAINT [PK_CargosSet]
+    PRIMARY KEY CLUSTERED ([Id] ASC);
+GO
+
+-- Creating primary key on [Id] in table 'Cal_NominaSet'
+ALTER TABLE [dbo].[Cal_NominaSet]
+ADD CONSTRAINT [PK_Cal_NominaSet]
+    PRIMARY KEY CLUSTERED ([Id] ASC);
+GO
+
+-- Creating primary key on [Id] in table 'LicenciasSet'
+ALTER TABLE [dbo].[LicenciasSet]
+ADD CONSTRAINT [PK_LicenciasSet]
+    PRIMARY KEY CLUSTERED ([Id] ASC);
+GO
+
+-- Creating primary key on [Id] in table 'PermisosSet'
+ALTER TABLE [dbo].[PermisosSet]
+ADD CONSTRAINT [PK_PermisosSet]
+    PRIMARY KEY CLUSTERED ([Id] ASC);
+GO
+
+-- Creating primary key on [Id] in table 'VacacionesSet'
+ALTER TABLE [dbo].[VacacionesSet]
+ADD CONSTRAINT [PK_VacacionesSet]
+    PRIMARY KEY CLUSTERED ([Id] ASC);
+GO
+
+-- Creating primary key on [Id] in table 'Salida_EmpleadosSet'
+ALTER TABLE [dbo].[Salida_EmpleadosSet]
+ADD CONSTRAINT [PK_Salida_EmpleadosSet]
+    PRIMARY KEY CLUSTERED ([Id] ASC);
+GO
+
+-- --------------------------------------------------
+-- Creating all FOREIGN KEY constraints
+-- --------------------------------------------------
+
+-- Creating foreign key on [DepartamentosId] in table 'EmpleadosSet'
+ALTER TABLE [dbo].[EmpleadosSet]
+ADD CONSTRAINT [FK_DepartamentosEmpleados]
+    FOREIGN KEY ([DepartamentosId])
+    REFERENCES [dbo].[Departamentos]
+        ([Id])
+    ON DELETE NO ACTION ON UPDATE NO ACTION;
+GO
+
+-- Creating non-clustered index for FOREIGN KEY 'FK_DepartamentosEmpleados'
+CREATE INDEX [IX_FK_DepartamentosEmpleados]
+ON [dbo].[EmpleadosSet]
+    ([DepartamentosId]);
+GO
+
+-- Creating foreign key on [CargosId] in table 'EmpleadosSet'
+ALTER TABLE [dbo].[EmpleadosSet]
+ADD CONSTRAINT [FK_CargosEmpleados]
+    FOREIGN KEY ([CargosId])
+    REFERENCES [dbo].[CargosSet]
+        ([Id])
+    ON DELETE NO ACTION ON UPDATE NO ACTION;
+GO
+
+-- Creating non-clustered index for FOREIGN KEY 'FK_CargosEmpleados'
+CREATE INDEX [IX_FK_CargosEmpleados]
+ON [dbo].[EmpleadosSet]
+    ([CargosId]);
+GO
+
+-- Creating foreign key on [EmpleadosId] in table 'Cal_NominaSet'
+ALTER TABLE [dbo].[Cal_NominaSet]
+ADD CONSTRAINT [FK_EmpleadosCal_Nomina]
+    FOREIGN KEY ([EmpleadosId])
+    REFERENCES [dbo].[EmpleadosSet]
+        ([Id])
+    ON DELETE NO ACTION ON UPDATE NO ACTION;
+GO
+
+-- Creating non-clustered index for FOREIGN KEY 'FK_EmpleadosCal_Nomina'
+CREATE INDEX [IX_FK_EmpleadosCal_Nomina]
+ON [dbo].[Cal_NominaSet]
+    ([EmpleadosId]);
+GO
+
+-- Creating foreign key on [EmpleadosId] in table 'LicenciasSet'
+ALTER TABLE [dbo].[LicenciasSet]
+ADD CONSTRAINT [FK_EmpleadosLicencias]
+    FOREIGN KEY ([EmpleadosId])
+    REFERENCES [dbo].[EmpleadosSet]
+        ([Id])
+    ON DELETE NO ACTION ON UPDATE NO ACTION;
+GO
+
+-- Creating non-clustered index for FOREIGN KEY 'FK_EmpleadosLicencias'
+CREATE INDEX [IX_FK_EmpleadosLicencias]
+ON [dbo].[LicenciasSet]
+    ([EmpleadosId]);
+GO
+
+-- Creating foreign key on [EmpleadosId] in table 'PermisosSet'
+ALTER TABLE [dbo].[PermisosSet]
+ADD CONSTRAINT [FK_EmpleadosPermisos]
+    FOREIGN KEY ([EmpleadosId])
+    REFERENCES [dbo].[EmpleadosSet]
+        ([Id])
+    ON DELETE NO ACTION ON UPDATE NO ACTION;
+GO
+
+-- Creating non-clustered index for FOREIGN KEY 'FK_EmpleadosPermisos'
+CREATE INDEX [IX_FK_EmpleadosPermisos]
+ON [dbo].[PermisosSet]
+    ([EmpleadosId]);
+GO
+
+-- Creating foreign key on [EmpleadosId] in table 'VacacionesSet'
+ALTER TABLE [dbo].[VacacionesSet]
+ADD CONSTRAINT [FK_EmpleadosVacaciones]
+    FOREIGN KEY ([EmpleadosId])
+    REFERENCES [dbo].[EmpleadosSet]
+        ([Id])
+    ON DELETE NO ACTION ON UPDATE NO ACTION;
+GO
+
+-- Creating non-clustered index for FOREIGN KEY 'FK_EmpleadosVacaciones'
+CREATE INDEX [IX_FK_EmpleadosVacaciones]
+ON [dbo].[VacacionesSet]
+    ([EmpleadosId]);
+GO
+
+-- Creating foreign key on [EmpleadosId] in table 'Salida_EmpleadosSet'
+ALTER TABLE [dbo].[Salida_EmpleadosSet]
+ADD CONSTRAINT [FK_EmpleadosSalida_Empleados]
+    FOREIGN KEY ([EmpleadosId])
+    REFERENCES [dbo].[EmpleadosSet]
+        ([Id])
+    ON DELETE NO ACTION ON UPDATE NO ACTION;
+GO
+
+-- Creating non-clustered index for FOREIGN KEY 'FK_EmpleadosSalida_Empleados'
+CREATE INDEX [IX_FK_EmpleadosSalida_Empleados]
+ON [dbo].[Salida_EmpleadosSet]
+    ([EmpleadosId]);
+GO
+
+-- --------------------------------------------------
+-- Script has ended
+-- --------------------------------------------------
