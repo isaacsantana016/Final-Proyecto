4a4db4e88fc82c4302dd87ec3a6a3cb518a8b969
isaacsantana016 <45479320+isaacsantana016@users.noreply.github.com>
2019-04-09 02:23:44 -0400

    sin las clases

--- Final-Programacion/Final-Programacion/App_Start/RouteConfig.cs
+++ Final-Programacion/Final-Programacion/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Final_Programacion
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Empleados", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
