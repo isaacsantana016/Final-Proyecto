3a52cbcb3dc417153e2e5f0d1a58e57a860c7a48
isaacsantana016 <45479320+isaacsantana016@users.noreply.github.com>
2019-04-09 01:46:33 -0400

    Agregue el modelo

---
+++ Final-Programacion/Final-Programacion/Models/EmpleadoContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace Final_Programacion.Models
+{
+    public class EmpleadoContext: DbContext
+    {
+        public DbSet<Empleados> Empleados { get; set; }
+    }
+}
